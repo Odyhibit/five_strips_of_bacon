3436c97842738d2357a89357896239e5aaf14b84
Odyhibit <josh.2990@gmail.com>
2023-03-08 23:54:01 -0600

    tkinter gui cipher program. Markdown text modifications. It uses 5 separate bacon ciphers to create ~~steganographic~~ hidden text.

---
+++ bacon.py
@@ -0,0 +1,56 @@
+original_bacon_dictionary = {"a": "00000",
+                             "b": "00001",
+                             "c": "00010",
+                             "d": "00011",
+                             "e": "00100",
+                             "f": "00101",
+                             "g": "00110",
+                             "h": "00111",
+                             "i": "01000",
+                             "j": "01000",
+                             "k": "01001",
+                             "l": "01010",
+                             "m": "01011",
+                             "n": "01100",
+                             "o": "01101",
+                             "p": "01110",
+                             "q": "01111",
+                             "r": "10000",
+                             "s": "10001",
+                             "t": "10010",
+                             "u": "10011",
+                             "v": "10011",
+                             "w": "10100",
+                             "x": "10101",
+                             "y": "10110",
+                             "z": "10111"}
+
+
+# create a bit_mask for the hidden_text
+def get_bit_mask(hide_me, dictionary_choice):
+    mask = ""
+    for char in hide_me:
+        if char in dictionary_choice:
+            mask += dictionary_choice[char]
+    return mask
+
+def process_char(letter: str, secret_bin_str: str) -> str:
+    output, this_letter = "", ""
+    prefix = ""
+    if secret_bin_str[0] == "1":
+        prefix += "*"
+    if secret_bin_str[1] == "1":
+        prefix += "**"
+    if secret_bin_str[2] == "1":
+        prefix += "~~"
+    if secret_bin_str[4] == "1":
+        prefix += "__"
+    if secret_bin_str[3] == "1":
+        this_letter = str(letter).upper()
+    else:
+        this_letter = str(letter)
+    return output + prefix + this_letter + prefix[::-1] + "\u200B"
+
+#print(process_char("H", "10001"))
+#print("Hello World!")
+#print(get_bit_mask("Hello_World!".lower(), original_bacon_dictionary))
---
+++ main.py
@@ -0,0 +1,99 @@
+from tkinter import *
+from tkinter import ttk
+import bacon
+
+
+class MainWindow:
+
+    def __init__(self, root):
+        root.title("Five Strips of Bacon")
+        content = ttk.Frame(root)
+
+        def convert_hidden(e):
+            temp = hidden_text.get("1.0", "end-1c").upper()
+            hidden_text.delete("1.0", "end")
+            new_text = ""
+            for letter in temp:
+                if letter.isalpha():
+                    new_text += letter
+            hidden_text.insert("1.0", new_text)
+
+        def check_length(e):
+            cover_str = cover_text.get("1.0", "end-1c")
+            hidden_str = hidden_text.get("1.0", "end-1c")
+            cover = 1 if str(e.char).isalnum() else 0
+            for letter in cover_str:
+                if letter.isalnum():
+                    cover += 1
+            if cover >= len(hidden_str):
+                calc_button.config(state="normal")
+            else:
+                calc_button.config(state="disabled")
+
+
+        def calculate_cipher():
+            # BISCU
+            secret_bin_str = bacon.get_bit_mask(hidden_text.get("1.0", "end -1c").lower(),
+                                                bacon.original_bacon_dictionary)
+            cover_text_str = cover_text.get("1.0", "end -1c")
+            output, this_letter = "", ""
+            i, j = 0, 0
+            while i < len(cover_text_str):
+                if cover_text_str[i].isalnum() and (j * 5) < len(secret_bin_str):
+                    output += bacon.process_char(cover_text_str[i], secret_bin_str[j * 5: j * 5 + 5])
+                    j += 1
+                else:
+                    output += cover_text_str[i]
+                i += 1
+            binary_version = ""
+            for i, character in enumerate(secret_bin_str):
+                binary_version += character
+                if (i+1) % 5 == 0:
+                    binary_version += " "
+
+            bin_text.delete("1.0", "end")
+            bin_text.insert("1.0", binary_version)
+
+            cipher_text.delete("1.0", "end")
+            cipher_text.insert("1.0", output)
+
+
+        def copy_to_clipboard():
+            field_value = cipher_text.get("1.0", 'end-1c')
+            root.clipboard_clear()
+            root.clipboard_append(field_value)
+
+        hidden_label = ttk.Label(content, text="Hidden Text - Bacon cipher is alpha only")
+        hidden_text = Text(content, height=3)
+        cover_label = ttk.Label(content, text="Cover Text - Will remove punctuation")
+        cover_text = Text(content, height=3)
+        cipher_label = ttk.Label(content, text="Cipher Text")
+        cipher_text = Text(content, height=6)
+        bin_label = ttk.Label(content, text="Binary of bacon - Bold,Italic,Strikethrough,Capital,Underline")
+        bin_text = Text(content, height=6)
+        calc_button = Button(content, text="Cipher", command=calculate_cipher, state="disabled")
+        clip_button = Button(content, text="Copy cipher", command=copy_to_clipboard)
+
+        pad_x = 10
+        pad_y = 5
+
+        content.grid(column=0, row=0, padx=pad_x, pady=pad_y)
+        hidden_label.grid(column=0, row=0, columnspan=2, padx=pad_x, pady=pad_y)
+        hidden_text.grid(column=0, row=1, columnspan=2, padx=pad_x, pady=pad_y)
+        cover_label.grid(column=0, row=2, columnspan=2, padx=pad_x, pady=pad_y)
+        cover_text.grid(column=0, row=3, columnspan=2, padx=pad_x, pady=pad_y)
+        cipher_label.grid(column=0, row=4, columnspan=2, padx=pad_x, pady=pad_y)
+        cipher_text.grid(column=0, row=5, columnspan=2, padx=pad_x, pady=pad_y)
+        bin_label.grid(column=0, row=6, columnspan=2, padx=pad_x, pady=pad_y)
+        bin_text.grid(column=0, row=7, columnspan=2, padx=pad_x, pady=pad_y)
+        calc_button.grid(column=0, row=8, padx=pad_x, pady=pad_y)
+        clip_button.grid(column=1, row=8, padx=pad_x, pady=pad_y)
+
+        hidden_text.bind("<FocusOut>", convert_hidden)
+        cover_text.bind("<Key>", check_length)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    MainWindow(root)
+    root.mainloop()
