bb617591057e68e83c812db201264b28f3b3623e
Odyhibit <josh.2990@gmail.com>
2024-07-13 14:35:41 -0500

    second draft of unicode, know bugs worked out, needs further testing, and removal of print statements.

--- src/decode_five_strips_of_bacon.py
+++ src/decode_five_strips_of_bacon.py
@@ -129,6 +129,66 @@ def process_char(letter: str) -> str:
     return return_letter
 
 
+def process_char_unicode(letter: str) -> str:
+    """decodes the attributes of one letter, and returns a hidden character
+
+    letter - string containing modifier characters, and the cover character
+    returns - string that is one decoded character
+    """
+    if letter == " " or letter == "":
+        print("skipping because of '' or ' '")
+        return ""
+    if letter[0] in (" ", "\u2060"):
+        letter = letter[1:]
+
+    output = [0, 0, 0, 0, 0]
+    letter_bytes = bytearray()
+    letter_bytes.extend(letter.encode())
+    if len(letter) == 0:
+        return ""
+
+    if "\u0332" in letter:  # underline
+        #print("underline", end=" ")
+        output[4] = 1
+        letter.replace("\u0332", "")
+    if "\u0336" in letter:  # strikethrough
+        #print("strikethrough", end=" ")
+        output[2] = 1
+        letter.replace("\u0336", "")
+    letter_bytes = bytearray()
+    letter_bytes.extend(letter.encode())
+    print("letter:", letter, "letter0:", letter[0], letter_bytes, str(hex(ord(letter[0]))), len(letter), end=" ")
+    if 0x1d400 <= ord(letter[0]) < 0x1D41A:  # Bold Capital
+        #print("bold capital", end=" ")
+        output[0] = 1
+        output[3] = 1
+    if 0x1D41a <= ord(letter[0]) < 0x1D433:  # Bold small
+        #print("bold small", end=" ")
+        output[0] = 1
+    if 0x1D434 <= ord(letter[0]) < 0x1D44E:  # Italic Capital
+        #print("italic capital", end=" ")
+        output[1] = 1
+        output[3] = 1
+    if 0x1D44E <= ord(letter[0]) < 0x1D467 or (ord(letter[0]) == 0x210e):  # Italic small
+        #print("italic small", end=" ")
+        output[1] = 1
+
+    if 0x40 < ord(letter[0]) < 0x5b:  # capital
+        #print(" capital~ ", end=" ")
+        output[3] = 1
+    this_int = "".join(map(str, output))
+    print(letter, output, reverse_bacon_dictionary[this_int])
+
+    binary_string = "".join(map(str, output))
+    if binary_string in reverse_bacon_dictionary:
+        return_letter = reverse_bacon_dictionary[binary_string]
+    else:
+        return f"{binary_string} is not a valid bacon encoding."
+    if len(letter) == 1 and ord(letter) == 0xfeff:
+        return_letter = " "
+    #print(return_letter)
+    return return_letter
+
 def process_word(cover_text: str) -> str:
     """Split cover text on zero-width space. use process_letter() to decode each
     letter, and it's markdown characters.
@@ -176,8 +236,19 @@ def main():
 
     :return: the original plaintext where i,j=i  u,v=u due to the bacon cipher limitations
     """
-    cover_text = grab_clipboard()
-    print(decode_cover_text(cover_text))
+    #cover_text = grab_clipboard()
+    #print(decode_cover_text(cover_text))
+    ciphered_message = "S̶̲​𝑒​C̲​R̲​e̶​𝑡̶​ ⁠𝐒​e̶​𝐜̶̲​𝐑​⁠E̶̲​𝑡​S̲​ a̶​𝐫̲​⁠⁠E̶̲​ s̶​𝐞​c̶​﻿ret"
+    letter_list = ciphered_message.split("​")
+    message = ""
+    for letter in letter_list:
+        if letter[0] == " ":
+            print("Remove leading space")
+            letter = letter[1:]
+        message += process_char_unicode(letter)
+
+    print(message)
+    #print(decode_cover_text(ciphered_message))
 
 
 if __name__ == "__main__":
